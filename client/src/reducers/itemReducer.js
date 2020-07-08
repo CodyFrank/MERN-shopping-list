6ce985e59fa8c995dc2ca7f3a0f6822c971c8d49
Cody Frank <cody.frank30@gmail.com>
2020-07-08 15:00:10 -0400

    update reducer to add items to end of state array

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -26,7 +26,7 @@ export default function(state = initialState, action) {
         case ADD_ITEM:
             return {
                 ...state, 
-                items: [ action.payload, ...state.items ]
+                items: [ ...state.items, action.payload ]
             }
         default:
              return state
