7af51ae5c4bdb9acc12a1085c9f33773f1f9e2ac
Cody Frank <cody.frank30@gmail.com>
2020-07-13 16:35:10 -0400

    bug fix

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -29,7 +29,7 @@ router.post('/', (req, res) => {
 
                     jwt.sign(
                         { id: user.id },
-                        config.get('jwtSecret') || process.env.JWT_SECRET,
+                        process.env.JWT_SECRET || config.get('jwtSecret'),
                         { expiresIn: 3600 },
                         (err, token) => {
                             if(err) throw err
